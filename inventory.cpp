b78e01505b287d415a1ce01ad26883234d5f6182
Retro4646 <116062726+Retro4646@users.noreply.github.com>
2025-07-21 18:53:22 +0530

    cleanup

--- inventory.cpp
+++ inventory.cpp
@@ -1,77 +1,102 @@
-#include <iostream>
-#include <fsrream>
+#include<iostream>
+#include<fstream>
 using namespace std;
 
-
 class temp{
-    
-    string id,name,author;
+
+    string id,name,author,search;
     fstream file;
     public:
     void addBook();
     void showAll();
     void extractBook();
 
+}obj;
 
-};
+int main(){
 
-
-
-
-int main() {
-
-    cout<<"-------------------------------------------------"
-    cout<<"1-Show All Books";
-    cout<<"2-Extract Book";
-    cout<<"3-Add Books (ADMIN)";
-    cout<<"4-Exit";
-    cout<<"----------------------------------------------------";
-    cout<<"Enter Your Choice ::  ";
+    char choice;
+    cout<<"----------------------------------"<<endl;
+    cout<<"1-Show All Books"<<endl;
+    cout<<"2-Extract Book"<<endl;
+    cout<<"3-Add books(ADMIN)"<<endl;
+    cout<<"4-Exit"<<endl;
+    cout<<"----------------------------------"<<endl;
+    cout<<"Enter Your Choice :: ";
     cin>>choice;
 
-
     switch(choice){
-        case: '1':
-
+        case '1':
+            cin.ignore();
+            obj.showAll();
         break;
-        case: '2':
-
+        case '2':
+            cin.ignore();
+            obj.extractBook();
         break;
-        case: '3':
-
+        case '3':
+            cin.ignore();
+            obj.addBook();
         break;
-        case: '4':
-
+        case '4':
+            return 0;
         break;
         default:
-            cout<<"Invaild Choice........!";
+            cout<<"Invalid Choice...!";
     }
-
+    
 
     return 0;
 }
-//Admin panel
 void temp :: addBook(){
     cout<<"\nEnter Book ID :: ";
     getline(cin,id);
     cout<<"Enter Book Name :: ";
     getline(cin,name);
-    cout<<"Enter Book Author name :: ";
+    cout<<"Enter Book's Author name :: ";
     getline(cin,author);
 
-    file.open("bookData.txt", ios :: out | ios :: app); 
+    file.open("bookData.txt",ios :: out | ios :: app);
     file<<id<<"*"<<name<<"*"<<author<<endl;
     file.close();
 }
-
 void temp :: showAll(){
-    file.open("bookData.txt", ios :: in);
+    file.open("bookData.txt",ios :: in);
     getline(file,id,'*');
     getline(file,name,'*');
     getline(file,author,'\n');
-        cout<<"\n\n"
-        cout<<"\t\t Book Id \t\t Book Name \t\t Author's Name";
-    while(!file.eof()) {
-        cout<<"\t\t "<<id<<" \t\t "<<name<<" \t\t\t "<<author<<endl;
+        cout<<"\n\n";
+        cout<<"\t\t Book Id \t\t\t Book Name \t\t\t Author's Name"<<endl;
+    while(!file.eof()){
+        cout<<"\t\t "<<id<<" \t\t\t\t "<<name<<" \t\t\t "<<author<<endl;
+
+        getline(file,id,'*');
+    getline(file,name,'*');
+    getline(file,author,'\n');
     }
+    file.close();
+}
+void temp :: extractBook(){
+
+    showAll();
+    cout<<"Enter Book Id :: ";
+    getline(cin,search);
+
+    file.open("bookData.txt",ios :: in);
+    getline(file,id,'*');
+    getline(file,name,'*');
+    getline(file,author,'\n');
+
+    cout<<"\n\n";
+    cout<<"\t\t Book Id \t\t\t Book Name \t\t\t Author's Name"<<endl;
+    while(!file.eof()){
+        if(search == id){
+            cout<<"\t\t "<<id<<" \t\t\t "<<name<<" \t\t\t "<<author<<endl;
+            cout<<"Book Extracted Successfully...!";
+        }
+    getline(file,id,'*');
+    getline(file,name,'*');
+    getline(file,author,'\n');
+    }
+    file.close();
 }
